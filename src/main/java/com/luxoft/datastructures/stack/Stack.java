a5496345df40eedc3a915a9f0cd1bdcb02b8b42c
Trubin <ATrubin@kiev.luxoft.com>
2021-10-25 21:35:05 +0300

    Initial commit

---
+++ src/main/java/com/luxoft/datastructures/stack/Stack.java
@@ -0,0 +1,21 @@
+package com.luxoft.datastructures.stack;
+
+// ADT -> Abstract Data Type
+// LIFO -> last-in-fist-out
+public interface Stack {
+    void push(Object value);
+
+    Object pop();
+
+    Object peek();
+
+    boolean contains(Object value);
+
+    int size();
+
+    boolean isEmpty();
+
+    void clear();
+
+
+}
---
+++ src/test/java/com/luxoft/datastructures/stack/ArrayStackTest.java
@@ -0,0 +1,109 @@
+package com.luxoft.datastructures.stack;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayStackTest {
+
+    @Test
+    public void testPushAndPopWorkCorrectlyAndChangeSize() {
+        ArrayStack arrayStack = new ArrayStack();
+        arrayStack.push("A");
+        arrayStack.push("B");
+
+        assertEquals(2, arrayStack.size());
+        assertEquals("B", arrayStack.pop());
+        assertEquals("A", arrayStack.pop());
+        assertEquals(0, arrayStack.size());
+        assertTrue(arrayStack.isEmpty());
+    }
+
+    @Test
+    public void testPushOverInitialCapacityAndPopWorkCorrectlyAndChangeSize() {
+        ArrayStack arrayStack = new ArrayStack(2);
+        arrayStack.push("A");
+        arrayStack.push("B");
+        arrayStack.push("C");
+
+        assertEquals(3, arrayStack.size());
+        assertEquals("C", arrayStack.pop());
+        assertEquals("B", arrayStack.pop());
+        assertEquals("A", arrayStack.pop());
+
+        assertEquals(0, arrayStack.size());
+        assertTrue(arrayStack.isEmpty());
+    }
+
+    @Test
+    public void testPushAndPeek() {
+        ArrayStack arrayStack = new ArrayStack();
+        arrayStack.push("A");
+        arrayStack.push("B");
+
+        assertEquals(2, arrayStack.size());
+        assertEquals("B", arrayStack.peek());
+        assertEquals("B", arrayStack.peek());
+        assertEquals(2, arrayStack.size());
+    }
+
+    @Test
+    public void testIsEmptyReturnTrueOnNewStack() {
+        ArrayStack arrayStack = new ArrayStack();
+
+        assertTrue(arrayStack.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyReturnFalseOnStackWithData() {
+        ArrayStack arrayStack = new ArrayStack();
+        arrayStack.push("A");
+
+        assertFalse(arrayStack.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyReturnTrueOnStackAfterClear() {
+        ArrayStack arrayStack = new ArrayStack();
+        arrayStack.push("A");
+        arrayStack.push("B");
+
+        arrayStack.clear();
+
+        assertTrue(arrayStack.isEmpty());
+    }
+
+    @Test
+    public void testContainsReturnTrue() {
+        ArrayStack arrayStack = new ArrayStack();
+        arrayStack.push("A");
+        arrayStack.push("B");
+
+        assertTrue(arrayStack.contains(new String("A")));
+    }
+
+    @Test
+    public void testContainsReturnFalse() {
+        ArrayStack arrayStack = new ArrayStack();
+        arrayStack.push("A");
+        arrayStack.push("B");
+
+        assertFalse(arrayStack.contains("C"));
+    }
+
+    @Test
+    public void testContainsReturnFalseOnEmptyStack() {
+        ArrayStack arrayStack = new ArrayStack();
+
+        assertFalse(arrayStack.contains("C"));
+    }
+
+    @Test
+    public void testThrowIllegalStateExceptionWhenPopOnEmptyStack() {
+        ArrayStack arrayStack = new ArrayStack();
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            arrayStack.pop();
+        });
+    }
+}
